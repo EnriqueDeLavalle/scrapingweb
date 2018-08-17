928d2f0d963c00486a72b6e2e6015baa4a95a97a
Enrique De Lavalle <enriquedelavalle@gmail.com>
2018-08-16 19:25:43 -0500

    changes

--- src/main/java/com/belatrix/scrapingWeb/DAO/PageDAO.java
+++ src/main/java/com/belatrix/scrapingWeb/DAO/PageDAO.java
@@ -12,7 +12,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.SynchronousQueue;
 import java.util.logging.Logger;
 
 import com.belatrix.scrapingWeb.model.Page;
---
+++ src/main/java/com/belatrix/scrapingWeb/model/KeyWord.java
@@ -0,0 +1,25 @@
+package com.belatrix.scrapingWeb.model;
+
+public class KeyWord {
+	
+	private String word;
+
+	public KeyWord() {
+		super();
+	}
+
+	public KeyWord(String word) {
+		super();
+		this.word = word;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+	
+
+}
